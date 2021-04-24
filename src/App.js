57f4bca0a2e09eb68a5ffebe614348078e9aafb3
ajayw4003 <ajayw4003@gmail.com>
2021-04-24 08:40:46 +0530

    changed name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [photos, setPhotos] = useState([]);
   const [apiKey ] = useState("lRnpiqlWz8znjm3S0W2g3q_nH-YY_7vQ0vWGDSMyTcw")
   useEffect(() => {
-    fetch(`https://api.unsplash.com/search/photos?query=london&client_id=${apiKey}`)
+    fetch(`https://api.unsplash.com/search/photos?query=animals&client_id=${apiKey}`)
     .then(response => response.json())
     .then(result =>{
       console.log(result);
